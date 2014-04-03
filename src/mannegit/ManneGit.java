215639445e47932b4be0f0345fd7e612681b2935
919454589 <S518670@mail.nwmissouri.edu>
2014-04-03 14:21:07 -0500

    Manasa Manne

---
+++ src/mannegit/ManneGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mannegit;
+
+/**
+ *
+ * @author S518670
+ */
+public class ManneGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Manasa Manne");
+    }
+}
